13f71f35e1b1b871016a41134165bd5593d35941
zhangxinyang.1203 <zhangxinyang.1203@bytedance.com>
2022-10-28 11:40:02 +0800

    assignment1 basic solution

--- Assignment1/src/main.cpp
+++ Assignment1/src/main.cpp
@@ -26,6 +26,13 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
     // TODO: Implement this function
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
+    Eigen::Matrix4f translate;
+    translate << cos(rotation_angle/180.0*acos(-1)),-sin(rotation_angle/180.0*acos(-1)),0,0,
+        sin(rotation_angle/180.0*acos(-1)),cos(rotation_angle/180.0*acos(-1)),0,0,
+        0,0,1,0,
+        0,0,0,1;
+
+    model = translate * model;
 
     return model;
 }
@@ -40,6 +47,30 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     // TODO: Implement this function
     // Create the projection matrix for the given parameters.
     // Then return it.
+
+    Eigen::Matrix4f m;
+    m << zNear,0,0,0,
+        0,zNear,0,0,
+        0,0,zNear+zFar,-zNear*zFar,
+        0,0,1,0;
+    
+    float halve = eye_fov/2*MY_PI/180;
+    float top = tan(halve)*zNear;
+    float bottom = -top;
+    float right = top * aspect_ratio;
+    float left = -right;
+    Eigen::Matrix4f n,p;
+    n << 2/(right-left),0,0,0,
+        0,2/(top-bottom),0,0,
+        0,0,2/(zNear-zFar),0,
+        0,0,0,1;
+
+    p << 1,0,0,-(right+left)/2,
+        0,1,0,-(top+bottom)/2,
+        0,0,1,-(zFar+zNear)/2,
+        0,0,0,1;
+
+    projection = n * p * m;
 
     return projection;
 }
